5faca948a109eb2dfcfcfefe93abfbd19428887d
parkminwoo <parkminwoo1991@gmail.com>
2023-06-24 16:48:35 +0900

    build: 0.1.0

---
+++ translang/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2023 parkminwoo, MIT License
+from translang.core import TranslationService
+
+__all__ = ["TranslationService"]
+__version__ = "0.1.0"
+__author__ = "daniel park <parkminwoo1991@gmail.com>"
